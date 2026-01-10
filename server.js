94c9bcaa21c1816e8536054e3edb91d9795bd095
com <haydensixseven@gmail.com>
2026-01-10 16:25:10 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const app = express();
 // Use the PORT provided by Render, or default to 3000
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the current directory
-app.use(express.static(path.join(__dirname)));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Always serve index.html for any request (Single Page Application support)
+// Since index.html is inside /public, we point the join path there
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
